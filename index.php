e4a730846b5f15bd9f87cd0119df25f7b843b894
shodayuki <yshoda1227@gmail.com>
2021-04-18 22:45:18 +0900

    環境構築

---
+++ index.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CAT BELL | 安心・安全な猫専門ペットショップ</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/common.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js"></script>
+    <script src="./assets/js/script.js"></script>
+</head>
+
+<body class="drawer drawer--right">
+    <header class="header header--default js-header">
+        <div class="header__logo">
+            <img src="./assets/img/home/logo.svg" alt="CAT BELLロゴ画像">
+        </div>
+        <button type="button" class="drawer-toggle drawer-hamburger">
+            <span class="drawer-hamburger-icon"></span>
+        </button>
+        <nav class="header__nav drawer-nav" role="navigation">
+            <ul class="header__list drawer-menu">
+                <li class="header__item">
+                    <a href="#" class="drawer-menu-item">ペットを探す</a>
+                </li>
+                <li class="header__item">
+                    <a href="#" class="drawer-menu-item">お店を探す</a>
+                </li>
+                <li class="header__item">
+                    <a href="#" class="drawer-menu-item">ブログ一覧</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main class="main">
+        <div class="mainVisual js-mainVisual">
+            <div class="mainVisual__bgImg">
+                <div class="mainVisual__inner inner">
+                    <div class="mainVisual__wrap">
+                        <h1 class="mainVisual__title">猫と暮らそう</h1>
+                        <p class="mainVisual__text">安心・安全な猫専門ペットショップ</p>
+                        <div class="mainVisual__btnArea">
+                            <a href="#" class="mainVisual__btn">猫種一覧を見る</a>
+                            <a href="#" class="mainVisual__btn">お店を見る</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.mainVisual -->
+        <div class="news">
+            <div class="news__inner inner">
+                <div class="news__wrap">
+                    <div class="news__heading">お知らせ</div>
+                    <ul class="news__list">
+                        <li class="news__item">
+                            <div class="news__date">2022.02.24</div>
+                            <div class="news__title">
+                                <a href="#" class="news__link">猫にまつわるヒーリング効果とは！？プレゼントキャンペーンも実施中♪</a>
+                            </div>
+                        </li>
+                        <li class="news__item">
+                            <div class="news__date">2022.02.22</div>
+                            <div class="news__title">
+                                <a href="#" class="news__link">ねこの日★祝！レア種も仲間入り、ふれあいコーナーで癒されて♪</a>
+                            </div>
+                        </li>
+                        <li class="news__item">
+                            <div class="news__date">2022.02.02</div>
+                            <div class="news__title">
+                                <a href="#" class="news__link">【新宿店】ポイント2倍Day！この機会をお見逃しなく！</a>
+                            </div>
+                        </li>
+                        <li class="news__item">
+                            <div class="news__date">2022.01.04</div>
+                            <div class="news__title">
+                                <a href="#" class="news__link">新年SEAL！療法食10%OFF&ペットアイテムおまとめ買いでプレゼント</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /.news -->
+        <section class="findPets">
+            <div class="findPet__inner inner">
+                <div class="findPet__head">
+                    <h2 class="util__title">ペットを探す</h2>
+                    <p class="util__subTitle">Find a pet</p>
+                </div>
+                <div class="findPet__wrap">
+                    <ul class="findPet__list">
+                        <li class="findPet__item">
+                            <a href="#" class="findPet__link">
+                                <div class="findPet__catImg">
+                                    <img src="./assets/img/typeList/4columns/01.png" alt="スコティッシュフォールド">
+                                </div>
+                                <p class="findPet__catName">スコティッシュ<br>フォールド</p>
+                            </a>
+                        </li>
+                        <li class="findPet__item">
+                            <a href="#" class="findPet__link">
+                                <div class="findPet__catImg">
+                                    <img src="./assets/img/typeList/4columns/02.png" alt="アメリカンショートヘア">
+                                </div>
+                                <p class="findPet__catName">アメリカン<br>ショートヘア</p>
+                            </a>
+                        </li>
+                        <li class="findPet__item">
+                            <a href="#" class="findPet__link">
+                                <div class="findPet__catImg">
+                                    <img src="./assets/img/typeList/4columns/03.png" alt="メインクイーン">
+                                </div>
+                                <p class="findPet__catName">メインクイーン</p>
+                            </a>
+                        </li>
+                        <li class="findPet__item">
+                            <a href="#" class="findPet__link">
+                                <div class="findPet__catImg">
+                                    <img src="./assets/img/typeList/4columns/04.png" alt="エキゾチックショートヘア">
+                                </div>
+                                <p class="findPet__catName">エキゾチック<br>ショートヘア</p>
+                            </a>
+                        </li>
+                        <li class="findPet__item">
+                            <a href="#" class="findPet__link">
+                                <div class="findPet__catImg">
+                                    <img src="./assets/img/typeList/4columns/05.png" alt="ラグドール">
+                                </div>
+                                <p class="findPet__catName">ラグドール</p>
+                            </a>
+                        </li>
+                        <li class="findPet__item">
+                            <a href="#" class="findPet__link">
+                                <div class="findPet__catImg">
+                                    <img src="./assets/img/typeList/4columns/06.png" alt="ブリティッシュショートヘア">
+                                </div>
+                                <p class="findPet__catName">ブリティッシュ<br>ショートヘア</p>
+                            </a>
+                        </li>
+                        <li class="findPet__item">
+                            <a href="#" class="findPet__link">
+                                <div class="findPet__catImg">
+                                    <img src="./assets/img/typeList/4columns/07.png" alt="マンチカン">
+                                </div>
+                                <p class="findPet__catName">マンチカン</p>
+                            </a>
+                        </li>
+                        <li class="findPet__item">
+                            <a href="#" class="findPet__link">
+                                <div class="findPet__catImg">
+                                    <img src="./assets/img/typeList/4columns/08.png" alt="サイベリアン">
+                                </div>
+                                <p class="findPet__catName">サイベリアン</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="findPet__more">
+                    <a href="" class="util__link">猫種一覧を見る</a>
+                </div>
+            </div>
+        </section>
+        <!-- /.findPet -->
+        <section class="findStore">
+            <div class="findStore__inner inner">
+                <div class="findStore__head">
+                    <h2 class="util__title">お店を探す</h2>
+                    <p class="util__subTitle">Find a store</p>
+                </div>
+                <div class="findStore__wrap">
+                    <div class="findStore__map">
+                        <div class="findStore__area findStore__area--hokkaido"></div>
+                        <div class="findStore__area findStore__area--kyushu"></div>
+                        <div class="findStore__area findStore__area--shikoku"></div>
+                        <div class="findStore__area findStore__area--honshu1"></div>
+                        <div class="findStore__area findStore__area--honshu2"></div>
+                    </div>
+                    <div class="findStore__shop">
+                        <a href="#" class="findStore__link"><div class="findStore__name findStore__name--sapporo">札幌店</div></a>
+                        <a href="#" class="findStore__link"><div class="findStore__name findStore__name--miyagi">宮城店</div></a>
+                        <a href="#" class="findStore__link"><div class="findStore__name findStore__name--shinjuku">新宿店</div></a>
+                        <a href="#" class="findStore__link"><div class="findStore__name findStore__name--ishikawa">石川店</div></a>
+                        <a href="#" class="findStore__link"><div class="findStore__name findStore__name--umeda">梅田店</div></a>
+                        <a href="#" class="findStore__link"><div class="findStore__name findStore__name--shizuoka">静岡店</div></a>
+                        <a href="#" class="findStore__link"><div class="findStore__name findStore__name--fukuoka">福岡店</div></a>
+                        <a href="#" class="findStore__link"><div class="findStore__name findStore__name--kagoshima">鹿児島店</div></a>
+                    </div>
+                </div>
+                <div class="findStore__more">
+                    <a href="" class="util__link">店舗一覧を見る</a>
+                </div>
+            </div>
+        </section>
+        <!-- /.store -->
+        <section class="blog">
+            <div class="blog__inner inner">
+                <div class="blog__head">
+                    <h2 class="util__title">ブログ</h2>
+                    <p class="util__subTitle">Blog</p>
+                </div>
+                <div class="blog__wrap">
+                    <ul class="blog__list">
+                        <li class="blog__item">
+                            <a href="#" class="blog__link">
+                                <div class="blog__img">
+                                    <img src="./assets/img/blog/00.jpg" alt="">
+                                </div>
+                                <div class="blog__info">
+                                    <h3 class="blog__title">猫にまつわるヒーリング効果とは！？プレゼントキャンペーンも実施中♪</h3>
+                                    <p class="blog__date">2022.02.24</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="blog__item">
+                            <a href="#" class="blog__link">
+                                <div class="blog__img">
+                                    <img src="./assets/img/blog/01.jpg" alt="">
+                                </div>
+                                <div class="blog__info">
+                                    <h3 class="blog__title">ねこの日★祝！レア種も仲間入り、ふれあいコーナーで癒されて♪</h3>
+                                    <p class="blog__date">2022.02.22</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="blog__item">
+                            <a href="#" class="blog__link">
+                                <div class="blog__img">
+                                    <img src="./assets/img/blog/02.jpg" alt="">
+                                </div>
+                                <div class="blog__info">
+                                    <h3 class="blog__title">【新宿店】ポイント2倍Day！この機会をお見逃しなく！</h3>
+                                    <p class="blog__date">2021.12.24</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="blog__item">
+                            <a href="#" class="blog__link">
+                                <div class="blog__img">
+                                    <img src="./assets/img/blog/03.jpg" alt="">
+                                </div>
+                                <div class="blog__info">
+                                    <h3 class="blog__title">新年SEAL！療法食10%OFF&ペットアイテムおまとめ買いでプレゼント</h3>
+                                    <p class="blog__date">2021.12.24</p>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="blog__more">
+                    <a href="" class="util__link">ブログ一覧を見る</a>
+                </div>
+            </div>
+        </section>
+        <!-- /.blog -->
+        <div class="about">
+            <div class="about__inner inner">
+                <div class="about__head">
+                    <h2 class="util__title">サイトについて</h2>
+                    <p class="util__subTitle">About</p>
+                </div>
+                <div class="about__wrap">
+                    <div class="about__img">
+                        <img src="./assets/img/home/about_01.png" alt="">
+                    </div>
+                    <div class="about__body">
+                        <div class="about__title">ペットと人との笑顔ある未来の創造</div>
+                        <p class="about__text">
+                            家族の絆を深める、子供の情操教育、ヒーリング効果など、<br>
+                            ペットと暮らすメリットが証明されてきており、<br>
+                            それらの効果は人々の暮らしに必要不可欠な&quot;笑顔&quot;を<br>
+                            もたらすことができます。<br>
+                            CAT BELLは、ペットというかけがえのない生命を<br>
+                            お客様へご提供することで、笑顔ある未来を創造し、<br>
+                            より豊かな社会環境の構築に貢献いたします。
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.about -->
+    </main>
+    <footer class="footer">
+        <div class="footer__inner inner">
+            <div class="footer__wrap">
+                <div class="footer__logo">
+                    <img src="./assets/img/home/logo.svg" alt="CAT BELLロゴ画像">
+                </div>
+                <nav class="footer__nav">
+                    <ul class="footer__list">
+                        <li class="footer__item">
+                            <a href="">ペットを探す</a>
+                        </li>
+                        <li class="footer__item">
+                            <a href="">お店を探す</a>
+                        </li>
+                        <li class="footer__item">
+                            <a href="">ブログ一覧</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <p class="footer__copyRight">&copy; 2020-2022 CAT BELL Co., Ltd.</p>
+        </div>
+    </footer>
+</body>
+
+</html>
