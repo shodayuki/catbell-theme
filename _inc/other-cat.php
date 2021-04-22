51d816b9d74e74c395408de86d1975e6c7075519
shodayuki <yshoda1227@gmail.com>
2021-04-22 17:59:39 +0900

    他の注目している猫の共通ページのエラー解消

--- _inc/other-cat.php
+++ _inc/other-cat.php
@@ -9,13 +9,12 @@
         $taxonomys = get_terms($taxonomy_name);
 
         if (!is_wp_error($taxonomys) && count($taxonomys)) :
-
           foreach($taxonomys as $taxonomy) :
             $term_id = esc_html($taxonomy->term_id);
             $term_idsp = "cat_type_".$term_id;
             $photo = get_field('cat_type_img', $term_idsp);
 
-            if ($coutnUp < 4) {
+            if ($countUp < 4) {
               $terms = get_the_terms($post->ID, 'cat_type');
               foreach ($terms as $term) {
                 $term_link = get_term_link($term);
@@ -27,13 +26,13 @@
       <li class="anotherPet__item hover">
         <a href="<?php echo get_permalink(get_page_by_path('cat_type')->ID); ?><?php echo esc_html($taxonomy->slug); ?>" class="anotherPet__itemLink">
           <div class="anotherPet__catImg">
-            <img src="<?php echo $photo ?>" alt="<?php echo esc_html($taxonomy->name); ?>">
+            <img src="<?php echo $photo; ?>" alt="<?php echo esc_html($taxonomy->name); ?>">
           </div>
           <p class="anotherPet__catName"><?php echo esc_html($taxonomy->name); ?></p>
         </a>
       </li>
       <?php }}} ?>
-      <?php  $countUp++; endforeach; endif; ?>
+      <?php $countUp++; endforeach; endif; ?>
     </ul>
   </div>
 </div>
---
+++ taxonomy-hand_shop_type.php
@@ -0,0 +1,101 @@
+<?php get_header(); ?>
+<section class="catList">
+  <div class="catList__inner inner">
+    <!-- breadcrumb -->
+    <nav>
+      <ol class="breadcrumbs">
+        <li class="breadcrumbs__item"><a href="index.html" class="breadcrumbs__link">ホーム</a></li>
+        <li class="breadcrumbs__item"><a href="#" class="breadcrumbs__link">猫種一覧</a></li>
+        <li class="breadcrumbs__item">アメリカンショートヘア一覧</li>
+      </ol>
+    </nav>
+    <!-- /breadcrumb -->
+    <div class="catList__head">
+      <h2 class="catList__title"><?php echo single_term_title(); ?>の一覧</h2>
+    </div>
+    <ul class="cat__lists">
+      <?php
+        $type = get_query_var('hand_shop_type');
+        $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+        $args = array(
+          'tax_query' => array(
+            array(
+              'taxonomy' => 'cat_type',
+              'field' => 'slug',
+              'terms' => $type
+            ),
+          ),
+          'post_type' => 'cat',
+          'order' => 'DESC',
+          'paged' => $paged
+        );
+        $my_query = new WP_Query($args);
+        $pages = $my_query->max_num_pages;
+        $wp_query->max_num_pages = $pages;
+        if ($my_query->have_posts()) :
+        while ($my_query->have_posts()) : $my_query->the_post();
+      ?>
+      <li class="cat__list">
+        <a href="#" class="cast__list__img hover">
+          <?php foreach (SCF::get('猫ちゃんのサムネイル画像') as $field_name => $field_value) : ?>
+          <?php
+            $carousel_thumbnail = wp_get_attachment_image_src($field_value['cat_img'], 'large');
+            $carousel_thumbnail = esc_url($carousel_thumbnail[0]);
+            if (!$carousel_thumbnail) {
+              $carousel_thumbnail = 'https://placehold.jp/584x390.png';
+            }
+          ?>
+          <img src="<?php echo $carousel_thumbnail; ?>" alt="<?php echo the_title(); ?>">
+          <?php break; endforeach; ?>
+        </a>
+        <div class="cat__list__body">
+          <div class="cat__list__head">
+            <div href="#" class="cat__list__label hover">
+              <?php
+                $post_object = get_field('cat_shop');
+                echo get_the_title($post_object);
+              ?>
+            </div>
+            <div href="#" class="cat__list__title"><?php echo the_title(); ?></div>
+          </div>
+          <dl class="cat__list__content">
+            <dt>生年月日</dt>
+            <dd><?php echo get_field('cat_birthday'); ?></dd>
+            <dt>性別</dt>
+            <dd>
+              <?php if (get_field('cat_sex') === 'men') {
+                echo "オス";
+              } else {
+                echo "メス";
+              } ?>
+            </dd>
+            <dt>生体価格</dt>
+            <dd><span class="cat__price"><?php echo get_field('cat_price'); ?></span>円（税抜）</dd>
+          </dl>
+          <div class="cat__list__footer">
+            <div class="cat__list__footer--store-more">
+              <a href="<?php echo get_permalink($post_object); ?>" class="more__link more--store hover">お取扱い店舗を見る</a>
+            </div>
+            <div class="cat__list__footer--cat__more">
+              <a href="<?php the_permalink(); ?>" class="more__link more--cat hover">この猫を詳しく見る</a>
+            </div>
+          </div>
+        </div>
+      </li>
+      <?php endwhile; ?>
+      <?php else : ?>
+        <p style="font-size: 16px">該当する猫ちゃんが見つかりませんでした。</p>
+			<?php endif; ?>
+    </ul>
+    <!-- pagenation -->
+    <?php get_template_part('_inc/pager'); ?>
+    <!-- /pagenation -->
+    <?php get_template_part('_inc/other-cat'); ?>
+
+  </div>
+</section>
+<!-- /.anotherPet -->
+<?php get_footer(); ?>
+</body>
+
+</html>
