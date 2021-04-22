cb3783352d45a1f029d327848f21e62199c363f6
shodayuki <yshoda1227@gmail.com>
2021-04-22 14:02:09 +0900

    他の猫の紹介セクションを共通化

---
+++ _inc/other-cat.php
@@ -0,0 +1,39 @@
+<div class="anotherPet__wrap">
+  <div class="anotherPet__inner">
+    <h3 class="catList__title">ほかにもこんな猫種が注目されています！</h3>
+    <ul class="anotherPet__list">
+      <li class="anotherPet__item hover">
+        <a href="anotherPet__itemLink">
+          <div class="anotherPet__catImg">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/typeList/4columns/01.png" alt="">
+          </div>
+          <p class="anotherPet__catName">スコティッシュ<br>フォールド</p>
+        </a>
+      </li>
+      <li class="anotherPet__item hover">
+        <a href="anotherPet__itemLink">
+          <div class="anotherPet__catImg">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/typeList/4columns/03.png" alt="">
+          </div>
+          <p class="anotherPet__catName">メインクイーン</p>
+        </a>
+      </li>
+      <li class="anotherPet__item hover">
+        <a href="anotherPet__itemLink">
+          <div class="anotherPet__catImg">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/typeList/4columns/04.png" alt="">
+          </div>
+          <p class="anotherPet__catName">エキゾチック<br>ショートヘア</p>
+        </a>
+      </li>
+      <li class="anotherPet__item hover">
+        <a href="anotherPet__itemLink">
+          <div class="anotherPet__catImg">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/typeList/4columns/05.png" alt="">
+          </div>
+          <p class="anotherPet__catName">ラグドール</p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
